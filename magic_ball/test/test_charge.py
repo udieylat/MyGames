6e9b2219f5ad8584c209ed1caadfd2b9aff7e841
udi <udi@ridewithvia.com>
2025-04-19 18:03:14 +0300

    test_ball_position

--- magic_ball/test/test_charge.py
+++ magic_ball/test/test_charge.py
@@ -4,16 +4,34 @@ from parameterized import parameterized
 
 from board import Board
 from cards.charge import Charge
-from models import PlayerSign
+from models import PlayerSign, BallPosition
 
 
 class TestCharge(unittest.TestCase):
+    def setUp(self):
+        self.charge = Charge()
+        self.board = Board()
+
     @parameterized.expand(PlayerSign.__members__.keys())
     def test_num_available_moves(self, player_sign: PlayerSign):
-        charge = Charge()
-        available_moves = charge.get_available_moves(
+        available_moves = self.charge.get_available_moves(
             player_sign=player_sign,
-            board=Board(),
+            board=self.board,
             card_index=0,
         )
         self.assertEqual(15, len(available_moves))
+
+    @parameterized.expand(PlayerSign.__members__.keys())
+    def test_ball_position(self, player_sign: PlayerSign):
+        available_moves = self.charge.get_available_moves(
+            player_sign=player_sign,
+            board=self.board,
+            card_index=0,
+        )
+        expected_ball_position = (
+            BallPosition.white
+            if player_sign == PlayerSign.black
+            else BallPosition.black
+        )
+        for move in available_moves:
+            self.assertEqual(move.result_ball_position, expected_ball_position)
