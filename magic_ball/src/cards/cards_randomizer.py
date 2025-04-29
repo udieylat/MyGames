06f11b6256c0f3ed979f0db24d0e145514964ae7
udi <udi@ridewithvia.com>
2025-04-29 18:28:48 +0300

    fix cards randomization, tests pass

--- magic_ball/src/cards/cards_randomizer.py
+++ magic_ball/src/cards/cards_randomizer.py
@@ -24,6 +24,8 @@ class CardsRandomizer:
         # TODO: receive CardsConfig with num white/black cards to randomize
         white_card_names: list[str] | None = None,
         black_card_names: list[str] | None = None,
+        num_white_cards: int = 3,
+        num_black_cards: int = 3,
         cards_pull: list[Card] | None = None,
     ) -> tuple[list[Card], list[Card]]:
         if not cards_pull and cards_pull is not None:
@@ -34,41 +36,54 @@ class CardsRandomizer:
             if cards_pull is None
             else cards_pull[:]
         )
+        name_to_card = {
+            card.name: card
+            for card in cards_pull
+        }
         if not white_card_names and not black_card_names:
             white_cards = cls._randomize_from_pull(
                 cards_pull=cards_pull,
-                ignore_card_names=[],
+                num_cards=num_white_cards,
+                ignore_cards=[],
             )
             black_cards = cls._randomize_from_pull(
                 cards_pull=cards_pull,
-                ignore_card_names=white_card_names,
+                num_cards=num_black_cards,
+                ignore_cards=white_cards,
             )
         elif white_card_names and not black_card_names:
             white_cards = cls._get_cards(
                 card_names=white_card_names,
                 cards_pull=cards_pull,
+                total_num_cards=num_white_cards,
             )
             black_cards = cls._randomize_from_pull(
                 cards_pull=cards_pull,
-                ignore_card_names=white_card_names,
+                num_cards=num_black_cards,
+                ignore_cards=white_cards,
             )
         elif not white_card_names and black_card_names:
             black_cards = cls._get_cards(
                 card_names=black_card_names,
                 cards_pull=cards_pull,
+                total_num_cards=num_black_cards,
             )
             white_cards = cls._randomize_from_pull(
                 cards_pull=cards_pull,
-                ignore_card_names=black_card_names,
+                num_cards=num_white_cards,
+                ignore_cards=black_cards,
             )
         else:
             white_cards = cls._get_cards(
                 card_names=white_card_names,
                 cards_pull=cards_pull,
+                total_num_cards=num_white_cards,
             )
             black_cards = cls._get_cards(
                 card_names=black_card_names,
                 cards_pull=cards_pull,
+                total_num_cards=num_black_cards,
+                ignore_cards=white_cards,
             )
         return white_cards, black_cards
 
@@ -76,13 +91,13 @@ class CardsRandomizer:
     def _randomize_from_pull(
         cls,
         cards_pull: list[Card],
-        ignore_card_names: list[str],
-        num_cards: int = 3,
+        num_cards: int,
+        ignore_cards: list[Card],
     ) -> list[Card]:
         all_valid_cards = [
             card
             for card in cards_pull
-            if card.name not in ignore_card_names
+            if card not in ignore_cards
         ]
         assert len(all_valid_cards) >= num_cards, "Cannot randomize enough cards"
         random.shuffle(all_valid_cards)
@@ -93,15 +108,27 @@ class CardsRandomizer:
         cls,
         card_names: list[str],
         cards_pull: list[Card],
+        total_num_cards: int,
+        ignore_cards: list[Card] = [],
     ) -> list[Card]:
+        assert total_num_cards >= len(card_names), "Total num cards is larger than input card names list"
         name_to_card = {
             card.name: card
             for card in cards_pull
         }
-        return [
+        cards = [
             name_to_card[card_name]
             for card_name in card_names
         ]
+        if total_num_cards == len(card_names):
+            return cards
+
+        cards += cls._randomize_from_pull(
+            cards_pull=cards_pull,
+            ignore_cards=cards + ignore_cards,
+            num_cards=total_num_cards - len(cards),
+        )
+        return cards
 
     @classmethod
     def _full_compendium(cls) -> list[Card]:
