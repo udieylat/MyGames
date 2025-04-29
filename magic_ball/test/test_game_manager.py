f02edec6eac83a2984719304133c0e9da107113d
udi <udi@ridewithvia.com>
2025-04-29 17:08:23 +0300

    fix fixed_game test

--- magic_ball/test/test_game_manager.py
+++ magic_ball/test/test_game_manager.py
@@ -78,8 +78,8 @@ class TestGameManager(unittest.TestCase):
         filename = os.path.dirname(os.path.abspath(__file__))
         gm = GameManager.from_config_filename(f"{filename}/../config/fixed_game.json")
         game_summary = gm.export_summary()
-        self.assertEqual(game_summary.winner, "black")
-        self.assertEqual(game_summary.num_white_moves, 5)
+        self.assertEqual(game_summary.winner, "white")
+        self.assertEqual(game_summary.num_white_moves, 7)
         self.assertEqual(game_summary.final_ball_position, "white")
 
     @classmethod
