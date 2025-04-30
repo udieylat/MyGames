9485b1827a3955d9fd86c1c59fab21e50ec0be29
udi <udi@ridewithvia.com>
2025-05-01 00:16:46 +0300

    simulate script

--- magic_ball/scripts/simulate.py
+++ magic_ball/scripts/simulate.py
@@ -4,15 +4,23 @@ import time
 from cards.compendium import Compendium
 from game_simulator import GameSimulator
 
-sim = GameSimulator.from_config_filename("config/ai_vs_ai.json")
+sim = GameSimulator.from_config_filename(
+	config_filename="config/ai_vs_ai.json",
+)
 card_to_summary = {}
 
 for card_name in Compendium.get_cards_names():
 	config = sim.clone_config()
-	config.white_cards = [card_name]
-	s = GameSimulator(config)
+	config.cards_config.white_card_names = [
+		card_name,
+	]
+	simulator = GameSimulator(
+		config=config,
+	)
 	print(f"{time.strftime('%c')}: {card_name}")
-	card_to_summary[card_name] = s.run(10000)
+	summary = simulator.run(10000)
+	print(f"W: {summary.num_white_wins}, D: {summary.num_draws}, B: {summary.num_black_wins}")
+	card_to_summary[card_name] = summary
 
 runtime_sec = sum(summary.runtime_sec for summary in card_to_summary.values())
 print(f"Full simulation runtime: {runtime_sec:.2f}")
@@ -26,4 +34,4 @@ results = {
 	for card_name, summary in card_to_summary.items()
 }
 
-open("results/sim.json", 'w').write(json.dumps(results, indent=2))
+open("results/new_sim.json", 'w').write(json.dumps(results, indent=2))
