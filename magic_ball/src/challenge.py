49163814896fe81c99a850298e62e5c291ac043d
udi <udi@ridewithvia.com>
2025-04-30 19:14:50 +0300

    challenge WIP 2

--- magic_ball/src/challenge.py
+++ magic_ball/src/challenge.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
+import random
+
 from cards.cards_config import CardsConfig
+from constants import DEFAULT_NUM_CARDS_PER_PLAYER
+from game_config import GameConfig
 from game_manager import GameManager
 from models import PlayerSign
-from players.player_config import PlayerConfig, PlayerType, ScoreMultipliers
+from players.player_config import PlayerConfig
 
 
 class Challenge:
@@ -14,13 +18,55 @@ class Challenge:
     ):
         self._opponent_config = opponent_config
         self._cards_config = cards_config
+        self._game_manager: GameManager | None = None
+        self._level = 0
 
     def start(
         self,
         player_sign: PlayerSign | None = None,
     ) -> GameManager:
-        player_config = PlayerConfig.human()
-        return self._gm
+        self._game_manager = GameManager.new(
+            config=self._to_game_config(
+                player_sign=player_sign,
+            ),
+        )
+        return self._game_manager
 
-    # def next_level(self) -> GameManager:
-    #     if self._gm._game_status
+    def next_level(self):
+        pass
+
+    def _to_game_config(
+        self,
+        player_sign: PlayerSign | None,
+    ) -> GameConfig:
+        if player_sign is None:
+            player_sign = random.choice(list(PlayerSign.__members__.keys()))
+
+        cards_config = self._cards_config.model_copy()
+        if player_sign == PlayerSign.white:
+            white_player = PlayerConfig.human()
+            black_player = self._opponent_config
+            cards_config.num_white_cards = DEFAULT_NUM_CARDS_PER_PLAYER
+            cards_config.num_black_cards = self._get_num_opponent_cards()
+        else:
+            white_player = self._opponent_config
+            black_player = PlayerConfig.human()
+            cards_config.num_white_cards = self._get_num_opponent_cards()
+            cards_config.num_black_cards = DEFAULT_NUM_CARDS_PER_PLAYER
+
+        return GameConfig(
+            white_player=white_player,
+            black_player=black_player,
+            cards_config=cards_config,
+        )
+
+    def _get_num_opponent_cards(self) -> int:
+        match self._level:
+            case 0:
+                return DEFAULT_NUM_CARDS_PER_PLAYER
+            case 1:
+                return 5
+            case 2:
+                return 7
+            case _:
+                return 10
