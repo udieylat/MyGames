771066814d28d6e2c919ef585583a7ef5db0c669
udi <udi@ridewithvia.com>
2025-04-30 15:11:09 +0300

    simulation script

--- magic_ball/scripts/simulate.py
+++ magic_ball/scripts/simulate.py
@@ -1,3 +1,6 @@
+import json
+import time
+
 from cards.compendium import Compendium
 from game_simulator import GameSimulator
 
@@ -8,6 +11,19 @@ for card_name in Compendium.get_cards_names():
 	config = sim.clone_config()
 	config.white_cards = [card_name]
 	s = GameSimulator(config)
-	print(card_name)
+	print(f"{time.strftime('%c')}: {card_name}")
 	card_to_summary[card_name] = s.run(10000)
 
+runtime_sec = sum(summary.runtime_sec for summary in card_to_summary.values())
+print(f"Full simulation runtime: {runtime_sec:.2f}")
+
+results = {
+	card_name: (
+		summary.num_white_wins,
+		summary.num_draws,
+		summary.num_black_wins,
+	)
+	for card_name, summary in card_to_summary.items()
+}
+
+open("results/sim.json", 'w').write(json.dumps(results, indent=2))
