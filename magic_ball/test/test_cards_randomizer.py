5f53905b9c0c5825e28d4cd5a7cdfbca43278d0f
udi <udi@ridewithvia.com>
2025-04-30 15:47:24 +0300

    test_draw_cards_num_cards

--- magic_ball/test/test_cards_randomizer.py
+++ magic_ball/test/test_cards_randomizer.py
@@ -27,3 +27,13 @@ class TestCardsRandomizer(unittest.TestCase):
         self.assertEqual(3, len(black_cards))
         self.assertNotIn(card_name, [card.name for card in white_cards])
         self.assertIn(card_name, [card.name for card in black_cards])
+
+    def test_draw_cards_num_cards(self):
+        white_cards, black_cards = CardsRandomizer.draw_cards(
+            num_white_cards=4,
+            num_black_cards=5,
+        )
+        self.assertEqual(4, len(white_cards))
+        self.assertEqual(5, len(black_cards))
+        common_card_names = {card.name for card in white_cards} & {card.name for card in black_cards}
+        self.assertSetEqual(set(), common_card_names)
