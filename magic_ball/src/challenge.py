06f03d69da76543e538d0685bc1f747a16ce166f
udi <udi@ridewithvia.com>
2025-04-30 20:35:39 +0300

    lock challenge hand and sign

--- magic_ball/src/challenge.py
+++ magic_ball/src/challenge.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import random
 
 from cards.cards_config import CardsConfig
+from cards.cards_randomizer import CardsRandomizer
 from constants import DEFAULT_NUM_CARDS_PER_PLAYER
 from game_config import GameConfig
 from game_manager import GameManager
@@ -13,29 +14,31 @@ from players.player_config import PlayerConfig, PlayerType
 class Challenge:
     def __init__(
         self,
+        player_sign: PlayerSign = PlayerSign.white,
         opponent_config: PlayerConfig = PlayerConfig.default_ai_opponent(),
-        cards_config: CardsConfig = CardsConfig(),
+        challenge_card_names: list[str] | None = None,
     ):
+        self._player_sign = player_sign
         self._opponent_config = opponent_config
-        self._cards_config = cards_config
+
+        if self._player_sign == PlayerSign.white:
+            self._cards_config = CardsConfig(
+                white_card_names=challenge_card_names,
+            )
+        else:
+            self._cards_config = CardsConfig(
+                black_card_names=challenge_card_names,
+            )
         self._game_manager: GameManager | None = None
         self._level = 0
-        # TODO: lock challenge hand (and sign??)
 
-    def start(
-        self,
-        player_sign: PlayerSign | None = None,
-    ) -> GameManager:
-        # TODO: keep hand, also keep sign??
+    def start(self) -> GameManager:
         self._game_manager = GameManager.new(
-            config=self._to_game_config(
-                player_sign=player_sign,
-            ),
+            config=self._to_game_config(),
         )
         return self._game_manager
 
     def next_level(self) -> GameManager | None:
-        # TODO: keep hand, also keep sign??
         if not self._game_is_won_by_human():
             print(f"Game is not won. Stay in level {self._level} and restart.")
             return self.start()
@@ -47,30 +50,31 @@ class Challenge:
         print(f"LEVEL UP: {self._level}")
         return self.start()
 
-    def my_level(self):
+    def challenge_status(self):
         print(f"Level: {self._level}")
         print(f"Number of opponent cards: {self._get_num_opponent_cards()}")
+        print(f"Playing as: {self._player_sign}")
+        print("Challenge cards:")
+        card_names = (
+            self._cards_config.white_card_names
+            if self._player_sign == PlayerSign.white
+            else self._cards_config.black_card_names
+        )
+        for i, card_name in enumerate(card_names):
+            print(f" {i+1}. {card_name}")
 
-    def _to_game_config(
-        self,
-        player_sign: PlayerSign | None,
-    ) -> GameConfig:
-        if player_sign is None:
-            player_sign = random.choice(list(PlayerSign.__members__.keys()))
-
+    def _to_game_config(self) -> GameConfig:
         cards_config = self._cards_config.model_copy()
-        if player_sign == PlayerSign.white:
+        if self._player_sign == PlayerSign.white:
             white_player = PlayerConfig.human()
             black_player = self._opponent_config
             cards_config.num_white_cards = DEFAULT_NUM_CARDS_PER_PLAYER
             cards_config.num_black_cards = self._get_num_opponent_cards()
-            # cards_config.white_card_names = [...]
         else:
             white_player = self._opponent_config
             black_player = PlayerConfig.human()
             cards_config.num_white_cards = self._get_num_opponent_cards()
             cards_config.num_black_cards = DEFAULT_NUM_CARDS_PER_PLAYER
-            # cards_config.black_card_names = [...]
 
         return GameConfig(
             white_player=white_player,
@@ -91,12 +95,12 @@ class Challenge:
 
     def _game_is_won_by_human(self) -> bool:
         if (
-            self._game_manager.game_config.white_player.type == PlayerType.human
+            self._player_sign == PlayerSign.white
             and self._game_manager.game_status in [GameStatus.white_win, GameStatus.white_defensive_win]
         ):
             return True
         if (
-            self._game_manager.game_config.black_player.type == PlayerType.human
+            self._player_sign == PlayerSign.black
             and self._game_manager.game_status in [GameStatus.black_win, GameStatus.black_defensive_win]
         ):
             return True
