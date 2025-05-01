58b4bb5ffeeb2b308f998bb64fd8533b0e6fce57
udi <udi@ridewithvia.com>
2025-05-01 13:23:36 +0300

    test_no_winning_available_card_move

--- magic_ball/test/test_tank.py
+++ magic_ball/test/test_tank.py
@@ -4,20 +4,36 @@ from parameterized import parameterized
 
 from board import Board
 from cards.tank import Tank
-from models import PlayerSign
+from models import PlayerSign, BallPosition
 
 
 class TestTank(unittest.TestCase):
     def setUp(self):
         self.tank = Tank()
-        self.board = Board.new()
 
     @parameterized.expand(PlayerSign.__members__.keys())
-    def test_num_available_card_moves(self, player_sign: PlayerSign):
+    def test_num_available_card_moves_starting_board(self, player_sign: PlayerSign):
         available_moves = self.tank.get_available_card_moves(
             player_sign=player_sign,
-            board=self.board,
+            board=Board.new(),
             card_index=0,
         )
         self.assertEqual(0, len(available_moves))
 
+    def test_no_winning_available_card_move(self):
+        board = Board(
+            board=[
+                [".", "W", ".", "W", "#"],
+                ["B", ".", ".", ".", "."],
+                ["W", ".", "W", ".", "."],
+                [".", ".", "W", ".", "."],
+                [".", "B", ".", "B", "B"],
+            ],
+            ball_position=BallPosition.middle,
+        )
+        available_moves = self.tank.get_available_card_moves(
+            player_sign=PlayerSign.white,
+            board=board,
+            card_index=0,
+        )
+        self.assertEqual(1, len(available_moves))  # Only C4->C3 (back push the white pawn)
